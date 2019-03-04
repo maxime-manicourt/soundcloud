1dc77a519e674418be5840016e142a595446c973
Maxime M <maxime.manicourt@gmail.com>
2019-03-04 10:02:35 +0100

    issues in view index fixed + tables migrated

--- app/Http/Controllers/MonControleur.php
+++ app/Http/Controllers/MonControleur.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class MonControleur extends Controller
 {
     public function index() {
-        return view("index.blade.php");
+        return view("index");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'MonControlleur@index');
+Route::get('/', 'MonControleur@index');
 
 
 Auth::routes();
