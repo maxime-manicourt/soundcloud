7840ce5f77243ebc511c9997b9b0badd1f29f660
Maxime M <maxime.manicourt@gmail.com>
2019-03-07 01:23:07 +0100

    npm install + comment require bootstrap in app.js + comment include bootstrap in app.scss or else npm watch won't load

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="last-tracks">
+        <h2>Les derniers titres</h2>
+    </div>
     <ul>
         @foreach($chansons as $c)
         <li>
